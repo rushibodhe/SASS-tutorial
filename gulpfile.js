ac2e6fc84204087a3722e003985bb86a77f27752
rushibodhe <bodhe.rushikesh1@gmail.com>
2023-06-02 08:06:33 +0530

    lect 3 ended shinobi structure and import orders

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 
 // Compile .scss into.css file:
 function buildStyles() {
-    return src('*.scss')
+    return src('shinobi/**/*.scss')
         .pipe(sass())
         .pipe(dest('css'))
 }
 
 // Watch for changes in .scss file:
 function watchTask() {
-    watch(['*.scss'], buildStyles)
+    watch(['shinobi/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
